b70b5cd3b050c27589a0d9314d0205fc7d5f4fd7
kim moore <121135528+ksmintx@users.noreply.github.com>
2022-12-20 16:40:25 -0600

    Update streamlit_app.py
    fixed typos

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
-streamlit.datafraqm(my_fruit_list)
+streamlit.dataframe(my_fruit_list)
 
 streamlit.title('New Healthy Diner')
 
@@ -12,4 +12,4 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.dataframe(my_fuit_list)
+streamlit.dataframe(my_fruit_list)
