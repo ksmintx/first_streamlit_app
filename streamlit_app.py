c553c26c486069265517826c8e77f4142f9e8b8c
kim moore <121135528+ksmintx@users.noreply.github.com>
2022-12-22 17:01:56 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,9 +23,10 @@ streamlit.dataframe(fruits_to_show)
 
 #new section to display fruityvice api response
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 streamlit.header("Fruityvice Fruit Advice!")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
 streamlit.text(fruityvice_response.json())
 
 
